d84c626412376e1e23b6115cfff1a55d5112ce89
ronshalee <sharon940329@gmail.com>
2023-09-30 18:05:15 +0900

    header - nav

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHashHistory } from "vue-router";
 import Home from "./Home.vue";
+import Movie from "./Movie.vue";
 import About from "./About.vue";
 
 export default createRouter({
@@ -8,6 +9,10 @@ export default createRouter({
     {
       path: "/",
       component: Home,
+    },
+    {
+      path: "/movie",
+      component: Movie,
     },
     {
       path: "/about",
