8dccb9be3a06b514af34616d574238154df85f12
ronshalee <sharon940329@gmail.com>
2023-10-01 00:51:50 +0900

    movieList, movieItem 컴포넌트 파일

--- src/routes/Home.vue
+++ src/routes/Home.vue
@@ -1,15 +1,19 @@
 <template>
   <Headline />
   <Search />
+  <MoviesList />
 </template>
 
 <script>
 import Headline from "../components/Headline.vue";
 import Search from "~/components/Search.vue";
+import MoviesList from "~/components/MovieList.vue";
+
 export default {
   components: {
     Headline,
     Search,
+    MoviesList,
   },
 };
 </script>
