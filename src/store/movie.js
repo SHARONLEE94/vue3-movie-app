4d4fa4e98c6afadfc41c2048deee3c30f8b6cf46
ronshalee <sharon940329@gmail.com>
2023-10-01 16:14:43 +0900

    영화 목록 ID 중복 제거 - lodash의 _uniqBy

--- src/store/movie.js
+++ src/store/movie.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import _uniqBy from "lodash/uniqBy";
 
 export default {
   namespaced: true,
@@ -33,7 +34,7 @@ export default {
       );
       const { Search, totalResults } = res.data;
       commit("updateState", {
-        movies: Search,
+        movies: _uniqBy(Search, "imdbID"),
         //message: "Hello world",
         //loading: true,
       });
@@ -52,7 +53,7 @@ export default {
           );
           const { Search } = res.data;
           commit("updateState", {
-            movies: [...state.movies, ...Search],
+            movies: [...state.movies, ..._uniqBy(Search, "imdbID")],
           });
         }
       }
