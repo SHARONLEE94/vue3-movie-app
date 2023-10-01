03d9c8872d327dd272dd7517b170843de49d46ee
ronshalee <sharon940329@gmail.com>
2023-10-01 16:04:39 +0900

    검색 결과 추가 요청

--- src/store/movie.js
+++ src/store/movie.js
@@ -25,7 +25,7 @@ export default {
   actions: {
     // Store의 Mutations를 실행할 때는 .commit()메소드를,
     // Actions를 실행할 때는 .dispatch(스토어 index.js에 연결된 모듈/액션(메소드)) 메소드를 사용
-    async searchMovies({ commit }, payload) {
+    async searchMovies({ state, commit }, payload) {
       const { title, type, number, year } = payload;
       const OMDB_API_KEY = "7035c60c";
       const res = await axios.get(
@@ -37,6 +37,25 @@ export default {
         //message: "Hello world",
         //loading: true,
       });
+      console.log(totalResults);
+      console.log(typeof totalResults);
+
+      const total = parseInt(totalResults, 10);
+      const pageLenght = Math.ceil(total / 10);
+
+      // 추가요청
+      if (pageLenght > 1) {
+        for (let page = 2; page <= pageLenght; page += 1) {
+          if (page > number / 10) break;
+          const res = await axios.get(
+            `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`
+          );
+          const { Search } = res.data;
+          commit("updateState", {
+            movies: [...state.movies, ...Search],
+          });
+        }
+      }
     },
   },
 };
